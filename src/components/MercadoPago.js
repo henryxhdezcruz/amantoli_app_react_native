52de674d220299a45f82dd96d3b912a57baf5b0c
Mijair-Nvte <mijairnvte@gmail.com>
2021-10-26 19:57:24 -0500

    Update MercadoPago.js

---
+++ src/components/MercadoPago.js
@@ -0,0 +1,13 @@
+<form>
+  <label>
+     Car:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Card" />
+
+  Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Card" />
+
+</form>
