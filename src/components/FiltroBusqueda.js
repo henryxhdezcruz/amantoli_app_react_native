1157400a8279fdf13ea68c1f863a74492d98dbb4
Floribeth26 <20181160@uthh.edu.mx>
2021-10-26 21:26:49 -0500

    Update FiltroBusqueda.js

--- src/components/FiltroBusqueda.js
+++ src/components/FiltroBusqueda.js
@@ -1,4 +1,12 @@
 <form>
     <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
+    <label>Filtro de busqueda</label>
     
 </form>
